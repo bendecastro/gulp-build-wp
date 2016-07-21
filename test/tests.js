4967437350471ebb013c9c17fd7068e7111a07e9
Ben de Castro <benjohnduarte@gmail.com>
2016-07-21 17:20:22 +0200

    Added mocha, chai and postcss

---
+++ test/tests.js
@@ -0,0 +1,12 @@
+;(function() {
+"use strict";
+
+var expect = require('chai').expect;
+
+describe('Mocha sanity check', function() {
+	it('is this test passing????', function() {
+		expect(true).to.be.ok;
+	});
+});
+
+}());
